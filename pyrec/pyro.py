572a65900509ad6a5483db88c418918c54f878c3
Kirsten Ziman <kirstenziman@kirstens-mbp.wireless.dartmouth.edu>
2017-02-14 10:23:30 -0500

    lists2pd and recall_matrix issues

--- pyrec/pyro.py
+++ pyrec/pyro.py
@@ -3,7 +3,7 @@
 
 # <h1>Pyrororobocop</h1>
 
-# In[1]:
+# In[43]:
 
 import pandas as pd
 from analysis import recall_matrix
@@ -39,22 +39,22 @@ class Pyro(object):
     """
 
 
-    def __init__(self, pres=pd.DataFrame(), features=pd.DataFrame(), rec=pd.DataFrame(), meta={}):
+    def __init__(self, pres=pd.DataFrame(), rec=pd.DataFrame(), meta={}):#features=pd.DataFrame(),
 
         self.pres=pres
-        self.features=features
+        #self.features=features
         self.rec=rec
         self.meta=meta
-        self._recall_mtx = recall_matrix(self.pres, self.rec)
+        #self._recall_mtx = recall_matrix(self.pres, self.rec)
 
 
 
-# In[2]:
+# In[44]:
 
 Pyro()
 
 
-# In[3]:
+# In[45]:
 
 from sqlalchemy import create_engine, MetaData, Table
 import json
@@ -74,7 +74,7 @@ from analysis import recall_matrix
 
 # <h1> Load data in dataframe</h1>
 
-# In[4]:
+# In[46]:
 
 db_url = "sqlite:///participants.db"
 table_name = 'turkdemo'
@@ -132,7 +132,7 @@ data_frame = pd.DataFrame(filtered_data)
 
 # <h1> Track Experiement Number</h1>
 
-# In[5]:
+# In[47]:
 
 db_url = "sqlite:///participants.db"
 table_name = 'turkdemo'
@@ -161,7 +161,7 @@ data_frame['exp_version']=version_col
 
 # <h1>Word Pool</h1>
 
-# In[6]:
+# In[48]:
 
 # read in stimulus library
 wordpool = pd.read_csv('cut_wordpool.csv')
@@ -169,7 +169,7 @@ wordpool = pd.read_csv('cut_wordpool.csv')
 
 # <h1>Select Experiment</h1>
 
-# In[7]:
+# In[73]:
 
 subids = list(data_frame[data_frame['listNumber']==15]['uniqueid'].unique())
 
@@ -205,7 +205,7 @@ print(exp1)
 
 # <h1>Processing Functions</h1>
 
-# In[8]:
+# In[50]:
 
 # this function takes the data frame and returns subject specific data based on the subid variable
 def filterData(data_frame,subid):
@@ -272,7 +272,7 @@ def computeListAcc(stimDict,recalledWords):
 
 # <h1>Create presentedWords and recalledWords</h1>
 
-# In[9]:
+# In[134]:
 
 experiment=[]
 all_recalled=[]
@@ -299,9 +299,27 @@ print(all_presented)
 
 # <h1>Create Presented & Recalled DataFrames</h1>
 
-# In[10]:
+# In[245]:
 
 def list2pd(all_data):
+    """
+    Makes multi-indexed dataframe of subject data 
+
+    Parameters
+    ----------
+    all_data : list of lists of strings
+        strings are either all presented or all recalled items, in the order of presentation or recall 
+        *should also work for presented / recalled ints and floats, if desired
+        
+
+    Returns
+    ----------
+    subs_list_of_dfs : multi-indexed dataframe 
+        dataframe of subject data (presented or recalled words/items), indexed by subject and list number
+        cell populated by the term presented or recalled in the position indicated by the column number
+
+    """
+    
     
     def make_multi_index(sub_data, isub):
         return pd.MultiIndex.from_tuples([(sub_num,lst_num) for lst_num,lst in enumerate(sub_data)], names = ['Subject', 'List'])
@@ -309,14 +327,45 @@ def list2pd(all_data):
     subs_list_of_dfs = [pd.DataFrame(sub_data, index=make_multi_index(sub_data, sub_num)) for sub_num,sub_data in enumerate(all_data)]
     return pd.concat(subs_list_of_dfs)
 
-list2pd(all_presented)
+datum = list2pd(all_presented)
+# print(type(list(datum.loc[0,0])))
+# x = list(datum.loc[0,1])
+# #[subj, list]
+# #print(x)
+
+# for row in datum.iterrows():
+# # #     for lst in subject.iterrows():
+#      print(list(row))
+#      print("space")
+
+# #     print
+# #     print
+# #     print()
+# #     print
+# #     print
+# #     print
+# datum[0][0].count()
+
+# #datum[column][Row][row]
+# print
+# datum
+
+len(datum[0][0].values.tolist())
+#datum.values.tolist()
+datum
 
 
 # <h1>Create Recall Matrix Dataframe</h1>
 
-# In[48]:
+# In[261]:
 
-def lists2rec_pd(all_presented, all_recalled):
+def lists2rec_pd(pres_df, rec_df):
+    
+    def pd2lists(pres_df, rec_df):
+        presented = pres_df.values.tolist()
+        recalled = rec_df.values.tolist()
+        
+        return(presented, recalled)
     
     def make_multi_index(sub_data, isub):
         return pd.MultiIndex.from_tuples([(sub_num,lst_num) for lst_num,lst in enumerate(sub_data)], names = ['Subject', 'List'])
@@ -330,25 +379,112 @@ def lists2rec_pd(all_presented, all_recalled):
         
         return(all_recall_mats)
     
-    rec_data = make_rec_mat(all_presented, all_recalled)
-    subs_list_of_recs = [pd.DataFrame(sub_data, index=make_multi_index(sub_data, sub_num)) for sub_num,sub_data in enumerate(rec_data)]
+    lsts = pd2lists(pres_df, rec_df)
+    rec_data = make_rec_mat(pres_df.values.tolist(), rec_df.values.tolist())
+    #subs_list_of_recs = [pd.DataFrame(sub_data, index=make_multi_index(sub_data, sub_num)) for sub_num,sub_data in enumerate(rec_data)]
     
-    return(pd.concat(subs_list_of_recs))
+    
+    #return(pd.concat(subs_list_of_recs))
+    return(pd.DataFrame(rec_data))
 
 
-# In[49]:
+# In[262]:
 
-lists2rec_pd(all_presented, all_recalled)
-
-
-# In[33]:
+lists2rec_pd(list2pd(all_presented), list2pd(all_recalled))
 
 
+# In[119]:
+
+#filtered_data
 
 
 # In[ ]:
 
 
+
+
+# In[172]:
+
+data = Pyro(list2pd(all_presented), list2pd(all_recalled))
+print(len(list2pd(all_presented).loc[2]))
+
+
+# In[268]:
+
+def recall_matrix2(pres_df, rec_df):
+    """
+    Computes recall matrix given df of presented and recalled words
+
+    Parameters
+    ----------
+    presented : multi indexed dataframe of strings
+      data indexed by subject number and list number
+      each cell contains the word presented in the location indicated by column number, as a string
+    
+    recalled : multi indexed dataframe of strings
+      data indexed by subject number and list number
+      each cell contains the word recalled in the location indicated by column number, as a string
+    
+    Returns
+    ----------
+    recall_matrix : multi indexed dataframe of ints and Nans 
+      indexed by subject and list number
+      each integer represents the presentation position of the recalled word in a given list in order of recall
+      0s represent recalled words not presented
+      negative ints represent words recalled from previous lists
+      Nans are filler for blank spaces where no word was recalled (number of columns equal to longest recall list)
+    
+    """
+
+    #x = list(datum.loc[0,1])
+    #[subj, list]
+    
+    ############
+    #def pd2list(pres_df, rec_df):
+    presented = pres_df.values.tolist()
+    recalled = rec_df.values.tolist()
+        
+        #return(presented, recalled)
+        
+        
+    recall_matrix=[[] for i in range(0,len(recalled))]#range(0, rec_df[0][0].count())]
+    #check that this gives number of subjects and not number of words per list.. 
+        
+
+    for idx,wordList in enumerate(recalled):
+        for word in wordList:
+            if word in presented[idx]:
+                recall_matrix[idx].append(presented[idx].index(word)+1)
+                #if the recalled word was presented, add to the recall matrix its presentation position
+
+            elif not word in presented[idx]:
+                if type(word) == float:
+                    recall_matrix[idx].append(np.nan)
+                for z in range(0,idx):
+                    if word in presented[z]:
+                        recall_matrix[idx].append(z-idx)
+                        #note, verify that by using [idx] here, we prevent positive value for "future" word
+                        #puts a negative number for word recalled from previous list
+                else:
+                    recall_matrix[idx].append(0)       
+            #if recalled word was not presented, append zero
+
+    return(pd.DataFrame(recall_matrix))
+    
+
+
+# <h1>Create EL Explabels and Last Labels</h1>
+
+# In[266]:
+
+#come back to this
+
+
+# <h1></h1>
+
+# In[269]:
+
+recall_matrix2(list2pd(all_presented), list2pd(all_recalled))
 
 
 # In[ ]:
