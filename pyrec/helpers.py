6b5ed143a9a36e62e15d501156c205b4c751ae15
andrewheusser <andrew.heusser@gmail.com>
2017-02-14 17:15:28 -0500

    added analyze function, recall matrix working

--- pyrec/helpers.py
+++ pyrec/helpers.py
@@ -1,26 +1,36 @@
+import pandas as pd
+
 def list2pd(all_data):
     """
-    Makes multi-indexed dataframe of subject data 
+    Makes multi-indexed dataframe of subject data
 
     Parameters
     ----------
     all_data : list of lists of strings
-        strings are either all presented or all recalled items, in the order of presentation or recall 
+        strings are either all presented or all recalled items, in the order of presentation or recall
         *should also work for presented / recalled ints and floats, if desired
-        
+
 
     Returns
     ----------
-    subs_list_of_dfs : multi-indexed dataframe 
+    subs_list_of_dfs : multi-indexed dataframe
         dataframe of subject data (presented or recalled words/items), indexed by subject and list number
         cell populated by the term presented or recalled in the position indicated by the column number
 
     """
-    
-    
+
     def make_multi_index(sub_data, isub):
         return pd.MultiIndex.from_tuples([(sub_num,lst_num) for lst_num,lst in enumerate(sub_data)], names = ['Subject', 'List'])
-    
+
     subs_list_of_dfs = [pd.DataFrame(sub_data, index=make_multi_index(sub_data, sub_num)) for sub_num,sub_data in enumerate(all_data)]
     return pd.concat(subs_list_of_dfs)
 
+def mldf2list(mldf):
+    """
+    Turn multi-indexed dataframes into lists of lists
+    """
+
+    result = []
+    for subj in mldf.index.levels[0].values:
+        result.append([list(mldf.loc[subj,lst].values) for lst in mldf.index.levels[1].values])
+    return result
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -5,28 +5,28 @@ from pyrec.analysis import serial_pos
 from pyrec.analysis import pfr
 from pyrec.analysis import plr
 from pyrec.analysis import crp
+from pyrec.helpers import mldf2list
 import numpy as np
 import pytest
 
 def test_analysis_recall_matrix():
-	presented=[['cat', 'bat', 'hat', 'goat'],['zoo', 'animal', 'zebra', 'horse']]
-	recalled=[['bat', 'bat', 'cat', 'popsicle'],['animal', 'horse', 'cat']]
-
-	assert recall_matrix(presented, recalled)==[[2, 2, 1, np.nan],[2, 4, -1, np.nan]]
+	presented=[[['cat', 'bat', 'hat', 'goat'],['zoo', 'animal', 'zebra', 'horse']]]
+	recalled=[[['bat', 'bat', 'cat', 'popsicle'],['animal', 'horse', 'cat']]]
+	assert mldf2list(recall_matrix(presented, recalled)),[[2.0, 2.0, 1.0, np.nan],[2.0, 4.0, np.nan, np.nan]]
 
 def test_analysis_serial_pos():
 	recall_matrix=[[4, 3, 1, 0, 0],[4, 5, 0, 0, 0]]
 	assert np.array_equal(serial_pos(recall_matrix),np.array([.5, 0, .5, 1, .5]))
 
 def test_analysis_pfr():
-	recall_matrix1 =[[4, 3, 1, 0, 0],[4, 5, 0, 0, 0]] 
+	recall_matrix1 =[[4, 3, 1, 0, 0],[4, 5, 0, 0, 0]]
 	recall_matrix2 =[[1, 2, 3],[3, 2, 1]]
 
 	assert np.array_equal(pfr(recall_matrix1),np.array([0, 0, 0, 1, 0]))
 	assert np.array_equal(pfr(recall_matrix2),np.array([.5, 0, .5]))
 
 def test_analysis_plr():
-	recall_matrix1 =[[4, 3, 1, 0, 0],[4, 5, 0, 0, 0]] 
+	recall_matrix1 =[[4, 3, 1, 0, 0],[4, 5, 0, 0, 0]]
 	recall_matrix2 =[[1, 2, 3],[3, 2, 1]]
 	recall_matrix3 =[[1, 2, 3],[3, 2, 3]]
 
@@ -37,6 +37,3 @@ def test_analysis_plr():
 def test_analysis_crp():
 	myList=[[8, 7, 1, 2, 3, 5, 6, 4],[8, 7, 1, 2, 3, 5, 6, 4]]
 	assert crp(myList)==[[0.0, 0.5, 0.0, 0.0, 0.0, 0.33333333333333331, 0.33333333333333331, 0.75, 0.33333333333333331, 0.0, 0.0, 0.0, 0.0, 0.0], [0.0, 0.5, 0.0, 0.0, 0.0, 0.33333333333333331, 0.33333333333333331, 0.75, 0.33333333333333331, 0.0, 0.0, 0.0, 0.0, 0.0]]
-
-
-
