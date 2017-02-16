36def20f2cb113f9c104e90fbc86cb77ea2302fd
Kirsten Ziman <kirstenziman@kirstens-mbp.wireless.dartmouth.edu>
2017-02-16 12:09:19 -0500

    first plots from tidy to seaborn ts plots

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -4,10 +4,15 @@ from pyrec.analysis import recall_matrix
 from pyrec.analysis import spc
 from pyrec.analysis import pfr
 from pyrec.analysis import plr
+<<<<<<< HEAD
 from pyrec.analysis import lag_crp
+=======
+from pyrec.analysis import crp
+>>>>>>> first plots from tidy to seaborn ts plots
 from pyrec.pyro import Pyro
 import numpy as np
 import pytest
+import pandas as pd
 
 presented=[[['cat', 'bat', 'hat', 'goat'],['zoo', 'animal', 'zebra', 'horse']]]
 recalled=[[['bat', 'cat', 'goat', 'hat'],['animal', 'horse', 'zoo']]]
@@ -34,6 +39,7 @@ def test_analysis_plr_listgroup():
 def test_analysis_lag_crp():
 	assert np.array_equal(lag_crp(pyro).values,np.array([[0.0, 0.0, 1.0, 0.0, 0.0, 1.0],[1.0, 0.0, 0.0, 0.0, 0.5, 0.0]]))
 
+<<<<<<< HEAD
 def test_analysis_lag_crp():
 	assert np.array_equal(plr(pyro, listgroup=[1,1]),[np.array([ 0.5,  0.,  0.5,  0.])])
 
@@ -47,3 +53,12 @@ def test_analysis_spc_multisubj():
 
 def test_analysis_spc_listgroup_subjgroup():
 	assert np.array_equal(spc(multisubj_pyro, subjgroup=[1,1], listgroup=[1,1]).values,[np.array([ 1. ,  1. ,  0.5,  1. ])])
+=======
+def test_pyro():
+	list1 = [[[1, 2], [3, 4]], [[5, 6], [7, 8]]]
+ 	list2 = [[[10, 20], [30, 40]], [[50, 60], [70, 80]]]
+
+ 	pyro = Pyro(pres = list1, rec = list2)
+ 	assert type(pyro.pres) == pd.core.frame.DataFrame
+ 	assert type(pyro.rec) == pd.core.frame.DataFrame
+>>>>>>> first plots from tidy to seaborn ts plots
