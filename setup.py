c1b08690679a7167fe970b545b3e5f73ff576567
andrewheusser <andrew.heusser@gmail.com>
2017-04-11 19:40:01 -0400

    trying a different way to get relative path

--- setup.py
+++ setup.py
@@ -19,5 +19,5 @@ setup(
     url='https://github.com/ContextLab/quail',
     license=license,
     packages=find_packages(exclude=('tests', 'docs')),
-    include_package_data=True, 
+    include_package_data=True,
 )
