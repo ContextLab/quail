63b2fae8e9478ac760f8ae5ec65e8f1704cbb920
andrewheusser <andrew.heusser@gmail.com>
2017-04-18 13:22:11 -0400

    setting up decode speech function

--- quail/__init__.py
+++ quail/__init__.py
@@ -3,14 +3,16 @@ from .egg import Egg
 from .analysis import analyze
 from .plot import plot
 from .helpers import stack_eggs
+from decode_speech import decode_speech
 
 class quail(object):
     '''quail module'''
 
-    def __init__(self,  Egg=Egg, load=load, load_example_data=load_example_data, analyze=analyze, plot=plot, stack_eggs=stack_eggs):
+    def __init__(self,  Egg=Egg, load=load, load_example_data=load_example_data, analyze=analyze, plot=plot, stack_eggs=stack_eggs, decode_speech=decode_speech):
         self.Egg = Egg
         self.load = load
         self.load_example_data = load_example_data
         self.analyze = analyze
         self.plot = plot
         self.stack_eggs = stack_eggs
+        self.decode_speech = decode_speech
---
+++ quail/decode_speech.py
@@ -0,0 +1,6 @@
+
+def decode_speech(folder_path):
+    """
+    Decode speech and return results
+    """
+    return folder_path
