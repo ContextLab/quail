7022e567f07db10dd07c338de30744d02b7ad8c5
andrewheusser <andrew.heusser@gmail.com>
2017-04-25 12:22:20 -0400

    fixed egg docs

--- quail/egg.py
+++ quail/egg.py
@@ -22,24 +22,34 @@ class Egg(object):
     the meta field is an optional dictionary that contains any details useful for
     identifying the egg object
 
-    Attributes
+    Parameters
     ----------
 
-    pres : pd.DataFrame
-        Dataframe containing the presented words.  Each row represents the presented words for a given list and each column
-        represents a list. The cells should be lowercase words. The index will be a multi-index, where the first level reprensents the subject number
+    pres : list (subjects) of lists (study lists) of lists (words) of strings
+        This is a nested list containing the presented words.  Internally, it will
+        be converted into a multi-index Pandas DataFrame. Each row represents
+        the presented words for a given list and each column represents a list.
+        The cells should be lowercase words. The index will be a multi-index,
+        where the first level reprensents the subject number and the second level
+        represents the list number.
+
+    rec : list (subjects) of lists (study lists) of lists (words) of strings
+        This is a nested list containing the presented words.  Internally, it will
+        be converted into a multi-index Pandas DataFrame. Each row represents
+        the recalled words for a given list and each column represents a list.
+        Each row represents the recalled words for a given list and each column
+        represents a list. The cells should be lowercase words. The index will
+        be a multi-index, where the first level reprensents the subject number
         and the second level represents the list number
 
-    rec : pd.DataFrame
-        Dataframe containing the words recalled.  Each row represents the recalled words for a given list and each column
-        represents a list.  Each row represents the recalled words for a given list and each column
-        represents a list. The cells should be lowercase words. The index will be a multi-index, where the first level reprensents the subject number
-        and the second level represents the list number
-
-    features : pd.DataFrame (optional)
-        Dataframe containing the features for presented words.  Each row represents the presented words for a given list and each column
-        represents a list. The cells should be a dictionary of features, where the keys are the name of the features, and the values are the feature values.
-        The index will be a multi-index, where the first level reprensents the subject number and the second level represents the list number
+    features : list (subjects) of lists (study lists) of lists (words) of strings
+        This is a nested list containing the presented words.  Internally, it will
+        be converted into a multi-index Pandas DataFrame. Each row represents the
+        presented words for a given list and each column represents a list. The
+        cells should be a dictionary of features, where the keys are the name of
+        the features, and the values are the feature values. The index will be a
+        multi-index, where the first level represents the subject number and the 
+        second level represents the list number.
 
     dist_funcs : dict (optional)
         A dictionary of custom distance functions for stimulus features.  Each key should be the name of a feature
