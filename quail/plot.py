445b0f03fe08b8018e382ea923a4d06d026d61cf
andrewheusser <andrew.heusser@gmail.com>
2017-04-27 09:09:15 -0400

    quail.plot can now handle lists.  WIP, need to also make the arguments lists

--- quail/plot.py
+++ quail/plot.py
@@ -13,7 +13,7 @@ def plot(data, subjgroup=None, subjname='Subject Group', listgroup=None,
 
     Parameters
     ----------
-    data : Pandas DataFrame
+    data : Pandas DataFrame or list of Pandas DataFrames
         The result of a quail analysis
 
     subjgroup : list of strings or ints
@@ -62,102 +62,106 @@ def plot(data, subjgroup=None, subjname='Subject Group', listgroup=None,
         An axis handle for the figure
 
     """
+    if type(data) is not list:
+        data=[data]
 
-    # if no grouping, set default to iterate over each list independently
-    subjgroup = subjgroup if subjgroup else data.index.levels[0].values
-    listgroup = listgroup if listgroup else data.index.levels[1].values
+    for d in data:
 
-    # convert to tiny and format for plotting
-    tidy_data = format2tidy(data, subjname, listname, subjgroup, analysis_type=data.analysis_type)
+        # if no grouping, set default to iterate over each list independently
+        subjgroup = subjgroup if subjgroup is not None else d.index.levels[0].values
+        listgroup = listgroup if listgroup is not None else d.index.levels[1].values
 
-    #plot!
-    if data.analysis_type is 'accuracy':
+        # convert to tiny and format for plotting
+        tidy_data = format2tidy(d, subjname, listname, subjgroup, analysis_type=d.analysis_type)
 
-        # set defaul style to bar
-        plot_style = plot_style if plot_style is not None else 'bar'
-        plot_type = plot_type if plot_type is not None else 'list'
+        #plot!
+        if d.analysis_type is 'accuracy':
 
-        if plot_style is 'bar':
-            plot_func = sns.barplot
-        elif plot_style is 'swarm':
-            plot_func = sns.swarmplot
-        elif plot_style is 'violin':
-            plot_func = sns.violinplot
+            # set defaul style to bar
+            plot_style = plot_style if plot_style is not None else 'bar'
+            plot_type = plot_type if plot_type is not None else 'list'
 
-        if plot_type is 'list':
-            ax = plot_func(data=tidy_data, x=listname, y="Accuracy", **kwargs)
-        elif plot_type is 'subject':
-            ax = plot_func(data=tidy_data, x=subjname, y="Accuracy", **kwargs)
-        elif plot_type is 'split':
-            ax = plot_func(data=tidy_data, x=subjname, y="Accuracy", hue=listname, **kwargs)
+            if plot_style is 'bar':
+                plot_func = sns.barplot
+            elif plot_style is 'swarm':
+                plot_func = sns.swarmplot
+            elif plot_style is 'violin':
+                plot_func = sns.violinplot
 
-    elif data.analysis_type is 'fingerprint':
+            if plot_type is 'list':
+                ax = plot_func(data=tidy_data, x=listname, y="Accuracy", **kwargs)
+            elif plot_type is 'subject':
+                ax = plot_func(data=tidy_data, x=subjname, y="Accuracy", **kwargs)
+            elif plot_type is 'split':
+                ax = plot_func(data=tidy_data, x=subjname, y="Accuracy", hue=listname, **kwargs)
 
-        # set default style to violin
-        plot_style = plot_style if plot_style is not None else 'violin'
-        plot_type = plot_type if plot_type is not None else 'list'
+        elif d.analysis_type is 'fingerprint':
 
-        if plot_style is 'bar':
-            plot_func = sns.barplot
-        elif plot_style is 'swarm':
-            plot_func = sns.swarmplot
-        elif plot_style is 'violin':
-            plot_func = sns.violinplot
+            # set default style to violin
+            plot_style = plot_style if plot_style is not None else 'violin'
+            plot_type = plot_type if plot_type is not None else 'list'
+
+            if plot_style is 'bar':
+                plot_func = sns.barplot
+            elif plot_style is 'swarm':
+                plot_func = sns.swarmplot
+            elif plot_style is 'violin':
+                plot_func = sns.violinplot
 
 
-        if plot_type is 'list':
-            ax = plot_func(data=tidy_data, x="Feature", y="Clustering Score", hue=listname, **kwargs)
-        elif plot_type is 'subject':
-            ax = plot_func(data=tidy_data, x="Feature", y="Clustering Score", hue=subjname, **kwargs)
-        else:
-            ax = plot_func(data=tidy_data, x="Feature", y="Clustering Score", **kwargs)
+            if plot_type is 'list':
+                ax = plot_func(data=tidy_data, x="Feature", y="Clustering Score", hue=listname, **kwargs)
+            elif plot_type is 'subject':
+                ax = plot_func(data=tidy_data, x="Feature", y="Clustering Score", hue=subjname, **kwargs)
+            else:
+                ax = plot_func(data=tidy_data, x="Feature", y="Clustering Score", **kwargs)
 
-    elif data.analysis_type is 'spc':
+        elif d.analysis_type is 'spc':
 
-        plot_type = plot_type if plot_type is not None else 'list'
+            plot_type = plot_type if plot_type is not None else 'list'
 
-        if plot_type is 'subject':
-            ax = sns.tsplot(data = tidy_data, time="Position", value="Proportion Recalled", unit="Subject", condition=subjname, **kwargs)
-        elif plot_type is 'list':
-            ax = sns.tsplot(data = tidy_data, time="Position", value="Proportion Recalled", unit="Subject", condition=listname, **kwargs)
-        ax.set_xlim(0,data.list_length-1)
+            if plot_type is 'subject':
+                ax = sns.tsplot(data = tidy_data, time="Position", value="Proportion Recalled", unit="Subject", condition=subjname, **kwargs)
+            elif plot_type is 'list':
+                ax = sns.tsplot(data = tidy_data, time="Position", value="Proportion Recalled", unit="Subject", condition=listname, **kwargs)
+            ax.set_xlim(0,d.list_length-1)
 
-    elif data.analysis_type is 'pfr':
+        elif d.analysis_type is 'pfr':
 
-        plot_type = plot_type if plot_type is not None else 'list'
+            plot_type = plot_type if plot_type is not None else 'list'
 
-        if plot_type is 'subject':
-            ax = sns.tsplot(data = tidy_data, time="Position", value="Probability of First Recall", unit="Subject", condition=subjname, **kwargs)
-        elif plot_type is 'list':
-            ax = sns.tsplot(data = tidy_data, time="Position", value="Probability of First Recall", unit="Subject", condition=listname, **kwargs)
-        ax.set_xlim(0,data.list_length-1)
+            if plot_type is 'subject':
+                ax = sns.tsplot(data = tidy_data, time="Position", value="Probability of First Recall", unit="Subject", condition=subjname, **kwargs)
+            elif plot_type is 'list':
+                ax = sns.tsplot(data = tidy_data, time="Position", value="Probability of First Recall", unit="Subject", condition=listname, **kwargs)
+            ax.set_xlim(0,d.list_length-1)
 
-    if data.analysis_type=='lagcrp':
+        if d.analysis_type=='lagcrp':
 
-        plot_type = plot_type if plot_type is not None else 'list'
+            plot_type = plot_type if plot_type is not None else 'list'
 
-        if plot_type is 'subject':
-            ax = sns.tsplot(data = tidy_data, time="Position", value="Conditional Response Probability", unit="Subject", condition=subjname, **kwargs)
-        elif plot_type is 'list':
-            ax = sns.tsplot(data = tidy_data, time="Position", value="Conditional Response Probability", unit="Subject", condition=listname, **kwargs)
-        ax.set_xlim(-5,5)
+            if plot_type is 'subject':
+                ax = sns.tsplot(data = tidy_data, time="Position", value="Conditional Response Probability", unit="Subject", condition=subjname, **kwargs)
+            elif plot_type is 'list':
+                ax = sns.tsplot(data = tidy_data, time="Position", value="Conditional Response Probability", unit="Subject", condition=listname, **kwargs)
+            ax.set_xlim(-5,5)
 
-    # add title
-    if title:
-        plt.title(title)
+        # add title
+        if title:
+            plt.title(title)
 
-    if legend is False:
-        try:
-            ax.legend_.remove()
-        except:
-            pass
+        if legend is False:
+            try:
+                ax.legend_.remove()
+            except:
+                pass
 
-    if ylim:
-        plt.ylim(ylim)
+        if ylim:
+            plt.ylim(ylim)
 
-    if save_path:
-        plt.savefig(save_path)
+        if save_path:
+            plt.savefig(save_path)
 
-    plt.show()
+        plt.show()
 
     return ax
