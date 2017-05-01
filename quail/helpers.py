a7903ebff9e9beff44fee074f55c4aa165d98a3c
andrewheusser <andrew.heusser@gmail.com>
2017-05-01 08:57:35 -0400

    fixed multiindex issue in crack egg

--- quail/helpers.py
+++ quail/helpers.py
@@ -174,17 +174,14 @@ def crack_egg(egg, subjects=None, lists=None):
 
     idx = pd.IndexSlice
     pres = egg.pres.loc[idx[subjects,lists],egg.pres.columns]
-    pres.index.set_levels(idx[subjects,lists], inplace=True)
     rec = egg.rec.loc[idx[subjects,lists],egg.rec.columns]
-    rec.index.set_levels(idx[subjects,lists], inplace=True)
 
-    pres = [pres.loc[sub,:].values.tolist() for sub in pres.index.levels[0].values]
-    rec = [rec.loc[sub,:].values.tolist() for sub in rec.index.levels[0].values]
+    pres = [pres.loc[sub,:].values.tolist() for sub in subjects]
+    rec = [rec.loc[sub,:].values.tolist() for sub in subjects]
 
     if all_have_features:
         features = egg.features.loc[idx[subjects,lists],egg.features.columns]
-        features.index.set_levels(idx[subjects,lists], inplace=True)
-        opts['features'] = [features.loc[sub,:].values.tolist() for sub in features.index.levels[0].values]
+        opts['features'] = [features.loc[sub,:].values.tolist() for sub in subjects]
 
     return Egg(pres=pres, rec=rec, **opts)
 
