4dbe8e703c4394ddf3f67e94bc1d7f1c63b2ef12
andrewheusser <andrew.heusser@gmail.com>
2017-05-15 11:07:34 -0400

    fixed bug with x axis limit, added xlim arg

--- quail/plot.py
+++ quail/plot.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 
 def plot(data, subjgroup=None, subjname='Subject Group', listgroup=None,
          listname='List', subjconds=None, listconds=None, plot_type=None,
-         plot_style=None, title=None, legend=True, ylim=None, save_path=None,
+         plot_style=None, title=None, legend=True, xlim=None, ylim=None, save_path=None,
          **kwargs):
     """
     General plot function that groups data by subject/list number and performs analysis.
@@ -57,6 +57,9 @@ def plot(data, subjgroup=None, subjname='Subject Group', listgroup=None,
 
     ylim : list of numbers
         A ymin/max can be specified by a list of the form [ymin, ymax]
+
+    xlim : list of numbers
+        A xmin/max can be specified by a list of the form [xmin, xmax]
 
     save_path : str
         Path to save out figure.  Include the file extension, e.g.
@@ -187,7 +190,7 @@ def plot(data, subjgroup=None, subjname='Subject Group', listgroup=None,
                 ax = sns.tsplot(data = tidy_data, time="Position", value="Proportion Recalled", unit="Subject", condition=subjname, **kwargs)
             elif plot_type is 'list':
                 ax = sns.tsplot(data = tidy_data, time="Position", value="Proportion Recalled", unit="Subject", condition=listname, **kwargs)
-            ax.set_xlim(0,attrs['list_length'])
+            ax.set_xlim(0,attrs['list_length']-1)
 
         elif attrs['analysis_type'] is 'pfr' or attrs['analysis_type'] is 'pnr':
             n=attrs['n']
@@ -199,7 +202,7 @@ def plot(data, subjgroup=None, subjname='Subject Group', listgroup=None,
                 ax = sns.tsplot(data = tidy_data, time="Position", value='Probability of Recall: Position ' + str(n), unit="Subject", condition=subjname, **kwargs)
             elif plot_type is 'list':
                 ax = sns.tsplot(data = tidy_data, time="Position", value='Probability of Recall: Position ' + str(n), unit="Subject", condition=listname, **kwargs)
-            ax.set_xlim(0,attrs['list_length'])
+            ax.set_xlim(0,attrs['list_length']-1)
 
         if attrs['analysis_type']=='lagcrp':
 
@@ -220,6 +223,9 @@ def plot(data, subjgroup=None, subjname='Subject Group', listgroup=None,
                 ax.legend_.remove()
             except:
                 pass
+
+        if xlim:
+            plt.xlim(xlim)
 
         if ylim:
             plt.ylim(ylim)
