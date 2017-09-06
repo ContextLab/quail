4d17d735fbae3342a19c81034fc63f634d2fd9dc
Student <Student@ebbinghaus.kiewit.dartmouth.edu>
2017-09-06 08:41:11 -0400

    removed build

--- build/lib/quail/__init__.py
+++
@@ -1,6 +0,0 @@
-from .load import load, load_example_data
-from .egg import Egg
-from .analysis import analyze
-from .plot import plot
-from .helpers import stack_eggs, crack_egg, recmat2egg, load_egg
-from decode_speech import decode_speech
