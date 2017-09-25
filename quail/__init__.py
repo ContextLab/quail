78ddae0f4d85c03ad124f79b880c1883c6328068
andrewheusser <andrew.heusser@gmail.com>
2017-09-25 10:46:25 -0400

    saving progress on new fingerprint code

--- quail/__init__.py
+++ quail/__init__.py
@@ -3,4 +3,5 @@ from .egg import Egg
 from .analysis import analyze
 from .plot import plot
 from .helpers import stack_eggs, crack_egg, recmat2egg, load_egg
-from decode_speech import decode_speech
+from .decode_speech import decode_speech
+from .fingerprint import Fingerprint, OptimalPresenter
