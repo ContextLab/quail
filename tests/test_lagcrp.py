c82e04b65369d47acb10c4c37f838ee2907358b8
andrewheusser <andrew.heusser@gmail.com>
2018-02-06 12:27:19 -0500

    multisubject test

--- tests/test_lagcrp.py
+++ tests/test_lagcrp.py
@@ -39,3 +39,16 @@ def test_lagcrp_smooth_euclidean_3d():
     egg = Egg(pres=presented, rec=recalled)
     np.allclose(egg.analyze('lagcrp', match='smooth', distance='euclidean').data.values, np.array([[np.nan, 980.875, 639.625, 442.54166667, 364.625, 380.875, 466.29166667, 595.875, np.nan,
     744.625, 660.875, 546.29166667, 425.875, 324.625, 267.54166667, 279.625, 385.875]]))
+
+def test_lagcrp_smooth_euclidean_3d_multisubject():
+    presented=[[[[10, 0, 0], [20, 0, 0], [30, 0, 0], [40, 0, 0], [50, 0, 0],
+                 [60, 0, 0], [70, 0, 0], [80, 0, 0]]],
+               [[[10, 0, 0], [20, 0, 0], [30, 0, 0], [40, 0, 0], [50, 0, 0],
+                            [60, 0, 0], [70, 0, 0], [80, 0, 0]]]]
+    recalled=[[[[10, 0, 0], [20, 0, 0], [30, 0, 0], [40, 0, 0], [50, 0, 0],
+                [60, 0, 0], [70, 0, 0], [80, 0, 0]]],
+              [[[10, 0, 0], [20, 0, 0], [30, 0, 0], [40, 0, 0], [50, 0, 0],
+                          [60, 0, 0], [70, 0, 0], [80, 0, 0]]]]
+    egg = Egg(pres=presented, rec=recalled)
+    np.allclose(egg.analyze('lagcrp', match='smooth', distance='euclidean',).data.values, np.array([[np.nan, 980.875, 639.625, 442.54166667, 364.625, 380.875, 466.29166667, 595.875, np.nan,
+    744.625, 660.875, 546.29166667, 425.875, 324.625, 267.54166667, 279.625, 385.875]]))
