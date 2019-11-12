39548b433684900f788f596487c653929225ee93
paxtonfitzpatrick <paxton.c.fitzpatrick.19@dartmouth.edu>
2019-11-12 15:49:14 -0500

    unpinned google-cloud-speech version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open('requirements.txt') as f:
     REQUIREMENTS = f.read().splitlines()
 
 EXTRAS_REQUIRE={
-    'speech-decoding': ["pydub", "google-cloud-speech<0.31dev,>=0.30.0", "google-auth"],
+    'speech-decoding': ["pydub", "google-cloud-speech", "google-auth"],
     'efficient-learning': ["sqlalchemy"],
 }
 
